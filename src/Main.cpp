ce73e5ea895daaf5607bdfb99a4ef3e357b4a82b
Adam McClenaghan <adam@mcclenaghan.co.uk>
2020-07-21 19:00:48 +0100

    Rough game loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
 
 int main(int argc, char* args[]) {
-  // TODO:
-  // Add game
-  std::cout << "Game is running..." << std::endl;
+
+  // Basics of the game loop:
+  // Process the input
+  // Update the game
+  // Render
+
+  Game* game = new Game();
+
+  game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+  while (game->IsRunning()) {
+      game ->ProcessInput();
+      game->Update();
+      game->Render();
+  }
+
+  game->Destroy();
+
   return 0;
 }
